91c25a31fd6b0696712dc5951f76c5208a14f82a
Micah Shute <micah.shute@gmail.com>
2020-03-05 17:55:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,16 +9,18 @@ def countdown(seconds)
   while seconds > 0 
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
+    countdown_with_sleep(seconds)
   end
   return "HAPPY NEW YEAR!"
 end
 
 
 def countdown_with_sleep(seconds)
-  while seconds > 0 
-    puts "#{seconds} SECOND(S)"
-    sleep 1
-    seconds -= 1
-  end
-  return "HAPPY NEW YEAR"
+  sleep(1)
+  # while seconds > 0 
+  #   puts "#{seconds} SECOND(S)"
+  #   sleep 1
+  #   seconds -= 1
+  # end
+  # return "HAPPY NEW YEAR"
 end
