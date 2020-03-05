0de0ce7a32f9d0cfed0aef987f0e74301e4a15eb
Micah Shute <micah.shute@gmail.com>
2020-03-05 17:51:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,9 @@
 # ACTIONS --> puts out number of seconds left for each loop --> puts a string
 
 def countdown(seconds)
-  
   while seconds > 0 
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
