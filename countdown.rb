3cce67b9489ca368eacbfad9b74440b2e150c95f
Micah Shute <micah.shute@gmail.com>
2020-03-05 17:53:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,7 +13,11 @@ def countdown(seconds)
 end
 
 
-def countdown_with_sleep
-  
-  
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)"
+    sleep(1)
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR"
 end
